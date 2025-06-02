020543fedc9e597c6b69080674542e2d6a85fadd
Abdurashid <fattokhovabdurashid@gmail.com>
2025-06-02 17:21:31 +0100

    added a resume section

---
+++ components/Resume/ResumeNew.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
+  const [numPages, setNumPages] = useState(null);
+  const pdfURL = "/Abdurashid_Fattokhov_Resume.pdf"; // from public folder
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+
+        {/* Top Download Button */}
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdfURL}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+
+        {/* CV Pages - Full Page 1 + Scroll to Page 2 */}
+        <Row style={{ justifyContent: "center", marginTop: "30px" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              width: "100%",
+              paddingBottom: "80px",
+            }}
+          >
+            <Document file={pdfURL} onLoadSuccess={onDocumentLoadSuccess}>
+              {Array.from(new Array(numPages), (el, index) => (
+                <Page
+                  key={`page_${index + 1}`}
+                  pageNumber={index + 1}
+                  scale={width > 786 ? 1.7 : 0.6}
+                  style={{
+                    marginBottom: "40px",
+                  }}
+                />
+              ))}
+            </Document>
+          </div>
+        </Row>
+
+        {/* Bottom Download Button */}
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdfURL}
+            target="_blank"
+            style={{ maxWidth: "250px", marginTop: "30px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
